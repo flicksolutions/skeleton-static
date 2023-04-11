7be1e9b979159a1eb724eb4a7be98da063712d9b
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-04-11 17:49:27 +0200

    add assets

--- src/lib/components/Gallery.svelte
+++ src/lib/components/Gallery.svelte
@@ -4,7 +4,20 @@
 </script>
 
 {#if browser}
+<div class="carousel-container">
     <Carousel>
         <slot></slot>
     </Carousel>
+</div>
 {/if}
+
+<style lang="scss">
+    .carousel-container {
+        max-width: 100%;
+
+        :global(img) {
+            max-width: 100%;
+            height: auto;
+        }
+    }
+</style>
