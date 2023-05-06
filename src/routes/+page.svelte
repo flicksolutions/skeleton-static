279c08c86c039bdaed67db59e8bdcaf9170d2f13
Sebastian Flick <sebastian.flick@wbkolleg.unibe.ch>
2023-05-06 23:45:58 +0200

    use gallery in content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,36 @@
 <script>
+	import BiggerPicture from 'bigger-picture/svelte';
 	import Content from '$lib/content/Content.md';
+	import { onMount } from 'svelte';
+	import { createWsrvSrc } from '$lib/functions';
+
+	let bp;
+
+	onMount(() => {
+		bp = BiggerPicture({
+			target: document.body
+		});
+		addListeners();
+	});
+
+	function addListeners() {
+		document.querySelectorAll('.lightbox').forEach((el) => {
+			el.addEventListener('click', openGallery);
+		});
+	}
+
+	function openGallery(e) {
+		e.preventDefault();
+		console.log(e);
+		bp.open({
+			el: e.target,
+			items: [
+				{
+					img: createWsrvSrc(e.target.attributes.src.value, { w: 9000, we: 'true' })
+				}
+			]
+		});
+	}
 </script>
 
 <Content />
